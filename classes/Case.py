ded14da4e85cc05b68f071881b961e3bd3e0cb60
nitchitos <arthurleclerc.corp1@gmail.com>
2026-06-09 11:14:04 +0200

    Classe Case complète -> Possible modifications à ajoutées plus tard

--- classes/Case.py
+++ classes/Case.py
@@ -1,32 +1,43 @@
-# Classe Case
+''' 
+-- Classe Case --
+'''
+
 class Case():
     def __init__(self, position: tuple ,valeur: int):
-        self.__position = position
-        self.__valeur = valeur
+        self.__position = position # position de la case
+        self.__valeur = valeur # valeur de la case
     
     # Setter
     def set_x(self, new_x: int):
+        # Change la position x de la case
         self.__position[0] = new_x
         
     def set_y(self, new_y: int):
+        # Change la position y de la case
         self.__position[1] = new_y
         
     def set_valeur(self, new_val: int):
+        # Change la valeur de la case
         self.__valeur = new_val    
     
-    
+# ------------------------------------------------------ #
+
     # Getter
     def get_x(self):
+        # Renvoie la position x de la case
         return self.__position[0]
     
     def get_y(self):
+        # Renvoie la position y de la case
         return self.__position[1]
     
     def get_value(self):
+        # Renvoie la valeur de la case
         return self.__valeur
     
+# ------------------------------------------------------ #    
     
-    # Autres méthodes
+    # Comparateurs
     def compare_valeur(self, valeur: int) -> bool:
         if self.get_value() == valeur:
             return True
@@ -38,6 +49,9 @@ class Case():
             return True
         else:
             return False
+        
+        
+        
     
     
     
