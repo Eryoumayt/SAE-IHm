5d28e098428cd4eaa91ebcf964b4c2bbb76394e7
nitchitos <arthurleclerc.corp1@gmail.com>
2026-06-12 09:50:53 +0200

    Liaison au fichier qss et à la font

--- classes/MenuDroite.py
+++ classes/MenuDroite.py
@@ -7,3 +7,25 @@ from PyQt6.QtGui import QFontDatabase
 class MenuDroite(QWidget):
     def __init__(self):
         pass
+    
+    
+
+# -------------- MAIN -------------- #
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    
+    # Liaison au fichier de la font (chemin relatif)
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    font_path = os.path.join(base_dir, "vue\qss\LuckiestGuy-Regular.ttf", "LuckiestGuy-Regular.ttf")
+    QFontDatabase.addApplicationFont(font_path)
+
+    # Liaison au fichier qss pour le style du menu à droite (chemin relatif)
+    qss_path = os.path.join(base_dir, "qss", "right_menu.qss")
+    with open(qss_path, "r") as f:
+        app.setStyleSheet(f.read())
+        
+    menu_droite = MenuDroite()
+    menu_droite.show()
+        
+    sys.exit(app.exec())
