ade7e2658c089719ac099a6b7fa840ce1fdc9e33
nitchitos <arthurleclerc.corp1@gmail.com>
2026-06-12 13:20:40 +0200

    Partie Main de MenuDroite.py mit en commentaire

--- classes/MenuDroite.py
+++ classes/MenuDroite.py
@@ -12,20 +12,20 @@ class MenuDroite(QWidget):
 
 # -------------- MAIN -------------- #
 
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
+# if __name__ == "__main__":
+#    app = QApplication(sys.argv)
     
     # Liaison au fichier de la font (chemin relatif)
-    base_dir = os.path.dirname(os.path.abspath(__file__))
-    font_path = os.path.join(base_dir, "vue\qss", "LuckiestGuy-Regular.ttf")
-    QFontDatabase.addApplicationFont(font_path)
+#    base_dir = os.path.dirname(os.path.abspath(__file__))
+#    font_path = os.path.join(base_dir, "vue\qss", "LuckiestGuy-Regular.ttf")
+#    QFontDatabase.addApplicationFont(font_path)
 
     # Liaison au fichier qss pour le style du menu à droite (chemin relatif)
-    qss_path = os.path.join(base_dir, "qss", "right_menu.qss")
-    with open(qss_path, "r") as f:
-        app.setStyleSheet(f.read())
+#    qss_path = os.path.join(base_dir, "qss", "right_menu.qss")
+#    with open(qss_path, "r") as f:
+#        app.setStyleSheet(f.read())
         
-    menu_droite = MenuDroite()
-    menu_droite.show()
+#    menu_droite = MenuDroite()
+#    menu_droite.show()
         
-    sys.exit(app.exec())
+#    sys.exit(app.exec())
