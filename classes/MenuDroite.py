96fa99f7355b30259412d3e74bad4fe55c7a883e
nitchitos <arthurleclerc.corp1@gmail.com>
2026-06-12 09:32:49 +0200

    Base pour le menu à droite

---
+++ classes/MenuDroite.py
@@ -0,0 +1,9 @@
+import sys
+import os
+from PyQt6.QtCore import Qt
+from PyQt6.QtWidgets import QApplication, QLabel, QWidget, QVBoxLayout, QPushButton
+from PyQt6.QtGui import QFontDatabase
+
+class MenuDroite(QWidget):
+    def __init__(self):
+        pass
